15ef3f83ac1ce9abee55f3b74ed7b73ea51054d9
aboredvaro <34165099+aboredvaro@users.noreply.github.com>
2022-11-10 07:50:56 +0100

    Demo solved

--- components/Home/Work.tsx
+++ components/Home/Work.tsx
@@ -1,4 +1,5 @@
-import { FiArrowUpRight } from 'react-icons/fi';
+import work from '../../data/work.json';
+import Workitem from './Workitem';
 
 export default function Work({}) {
 	return (
@@ -11,47 +12,12 @@ export default function Work({}) {
 					<h2 className="text-5xl">Work</h2>
 
 					<div className="flex w-full flex-col gap-16">
-						{/* WORK ITEM */}
-
-						<div className="flex w-full flex-col gap-6 border-t border-gray-300 py-8 md:flex-row md:gap-16">
-							<div className="flex flex-1 flex-col space-y-5">
-								<div className="flex flex-col items-start space-y-1">
-									<span className="font-semibold uppercase">Internxt</span>
-									<span className="text-sm text-gray-500">
-										July 2020&nbsp;&nbsp;—&nbsp;&nbsp; Present
-									</span>
-								</div>
-								<a
-									target="_blank"
-									href="https://internxt.com/"
-									className="flex flex-row items-center space-x-1 underline-offset-4 hover:underline"
-								>
-									<span>Visit site</span>
-									<FiArrowUpRight />
-								</a>
-							</div>
-
-							<div className="flex flex-1 flex-col space-y-1">
-								<span className="font-medium uppercase">
-									Lead Product Designer
-								</span>
-								<div className="flex flex-col space-y-4 text-gray-500">
-									<span>
-										Led the creation, documentation, and implementation of the
-										design system in Internxt, using Figma, Storybook, and
-										TailwindCSS, achieving a 60% reduction in frontend
-										development time and code review.
-									</span>
-									<span>
-										Led UX Research, UI prototyping, Service Design and UI
-										Design for multiple new products. Helped Frontend team
-										during the development and testing of the applications.
-									</span>
-								</div>
-							</div>
-						</div>
-
-						{/* END WORK ITEM */}
+						{work.map((job) => (
+							<Workitem
+								props={job}
+								key={`${job.company}-${job.dateRange.from}`}
+							/>
+						))}
 					</div>
 				</div>
 			</div>
